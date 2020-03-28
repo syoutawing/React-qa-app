00586f78470d1400e748a1a6e640016e171bb974
midorikawa <sm84652427@yahoo.co.jp>
2020-03-28 11:25:29 +0900

    #add

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import NavBar from './NavBar/NavBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <p>Work in progress.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
